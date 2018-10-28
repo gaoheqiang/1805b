3671c102c79c56835eca3efbe074d931d6614e03
gaoheqiang <1359675322@qq.com>
2018-10-28 12:53:22 +0800

    test

---
+++ springboot01-cache/src/main/java/cn/tedu/CatchAppp.java
@@ -0,0 +1,15 @@
+package cn.tedu;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages="cn.tedu.mapper")
+public class CatchAppp {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
